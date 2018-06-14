a05f3c2b16ce85e5e81a55a32704c59d79b1ee31
Unknown <tmfrl4392@naver.com>
2018-06-14 14:00:03 +0900

    Controller 완성

--- src/kh/web/controller/FrontController.java
+++ src/kh/web/controller/FrontController.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jasper.tagplugins.jstl.core.Catch;
+
 import kh.web.messagesDAO.MessagesDAO;
 import kh.web.messagesDTO.MessagesDTO;
 
@@ -19,7 +21,8 @@ public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	 //  ---------------- FrontEnd 연결  ------------------------
+	 try {
+		//  ---------------- FrontEnd 연결  ------------------------
 		String requestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		String command = requestURI.substring(contextPath.length());
@@ -64,6 +67,7 @@ public class FrontController extends HttpServlet {
 			// ----------- forward : 하나의 값을 공유 해야 할 경우
 			rd.forward(request, response);
 		}
+	}catch(Exception e){e.printStackTrace();}
 	}
 
 
